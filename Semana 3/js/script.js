bf450936fa5f1564555c4ae04365f9abd4c78edb
Starossta <javier.figueroat@usm.cl>
2024-09-02 15:51:02 -0400

    awslkdjalskjd

---
+++ Semana 3/js/script.js
@@ -0,0 +1,10 @@
+function saludo(){
+    //alert("aaa")
+    var mih1=document.getElementById("mih1")
+    var cajadetexto=document.getElementById("txtnombre")
+    mih1.innerHTML ="Hola " + cajadetexto.value
+}
+function chao(){
+    var cajadetexto =document.getElementById("txtnombre")
+    mih1.innerHTML ="Chao " + cajadetexto.value
+}
