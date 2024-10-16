b2f366bc8f040c713427da73ce219ff88ff9dbd4
javier <javier.figueroat@usm.cl>
2024-10-16 00:50:30 -0300

    views con el cert 1

--- Taller Lp/CER2-FIGUEROAJAVIER/TiendaVerde/app/views.py
+++ Taller Lp/CER2-FIGUEROAJAVIER/TiendaVerde/app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    titulo='inicio'
+    data ={
+        'titulo':titulo
+    }
+    return render(request,'index.html',data)
